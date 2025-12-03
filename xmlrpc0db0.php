3d2598818722cc2d6f6d7aa3546d81d6ca8c606e
veresfruzsina81-eng <veresfruzsina81@gmail.com>
2025-12-03 03:41:22 +0100

    FIX10 full static site

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://RENEWAL ZED HOME KFT..hu/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://RENEWAL ZED HOME KFT..hu/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://RENEWAL ZED HOME KFT..hu/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://RENEWAL ZED HOME KFT..hu/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://RENEWAL ZED HOME KFT..hu/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://RENEWAL ZED HOME KFT..hu/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
